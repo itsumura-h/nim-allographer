2124353c40691372f91183b6ee775a5b4a1c50dd
itsumura-h <dumblepy@gmail.com>
2019-10-02 01:51:46 +0900

    create makeConf command

--- project/src/base.nim
+++ project/src/base.nim
@@ -1,39 +1,6 @@
-import json, os, strutils, parsecfg
-import db_sqlite, db_mysql, db_postgres
+import json
 
 
-type DBObject*[T] = ref object
-  connectionInfo*: JsonNode
-  sqlite*: db_sqlite.DbConn
-  mysql*: db_mysql.DbConn
-  postgres*: db_postgres.DbConn
-  connection: T
+type RDB* = ref object of RootObj
   query*: JsonNode
   sqlStringSeq*: seq[string]
-
-
-# proc setConnection*(this: DBObject): DBObject =
-#   # specify conf path
-#   let workingDirPath = getCurrentDir()
-#   var confPathArray = workingDirPath.split("/")
-#   confPathArray.add(["conf", "db.ini"])
-#   let confPath = confPathArray.join("/")
-#   echo confPath
-
-#   # load conf
-#   let conf = loadConfig(confPath)
-#   let driver = conf.getSectionValue("", "driver")
-#   let conn = conf.getSectionValue("", "connection")
-#   let user = conf.getSectionValue("", "user")
-#   let password = conf.getSectionValue("", "password")
-#   let database = conf.getSectionValue("", "database")
-#   this.connectionInfo = %*{
-#     "driver": driver,
-#     "conn": conn,
-#     "user": user,
-#     "password": password,
-#     "database": database
-#   }
-#   return this
-
-# proc db*(this: DBObject, conn: proc): DBObject =
--- project/src/grammars.nim
+++ project/src/grammars.nim
@@ -1,6 +1,14 @@
 import base
 import json
 
-proc table*(this: DBObject, tableArg: string): DBObject =
-    this.query = %*{"table": tableArg}
-    return this
+proc table*(this: RDB, tableArg: string): RDB =
+  this.query = %*{"table": tableArg}
+  return this
+
+proc select*(this: RDB, columnsArg: varargs[string]): RDB =
+  if columnsArg.len == 0:
+    this.query["select"] = %["*"]
+  else:
+    this.query["select"] = %*columnsArg
+  
+  return this
