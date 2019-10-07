489ed16dfb0f00d43fa131624ca29ccdc326ac5a
itsumura-h <dumblepy@gmail.com>
2019-10-07 13:26:59 +0900

    add schema generator

--- example/models.nim
+++ example/models.nim
@@ -1,20 +1,24 @@
 import db_common
+import ../src/migration/model
 import ../src/migration/schemaBuilders
-
-
-type Model* = ref object of RootObj
-  name*: string
-  columns*: seq[DbColumn]
-
-proc migrate*(this:Model) =
-  echo repr this
-
 
 #=================================================================
 
-Model(
-  name:"users",
-  columns: @[
+# Model(
+#   name:"users",
+#   columns: @[
+#     Schema().bigIncrements("id"),
+#     Schema().bigInteger("bigInteger"),
+#     Schema().binary("binary"),
+#     Schema().char("char", 4),
+#     Schema().char("default char", 4, default=""),
+#     Schema().date("date"),
+#   ]
+# ).migrate()
+
+Model().new(
+  "users",
+  @[
     Schema().bigIncrements("id"),
     Schema().bigInteger("bigInteger"),
     Schema().binary("binary"),
---
+++ src/migration/columnGenerator.nim
@@ -0,0 +1,33 @@
+import os, parsecfg, strformat
+
+proc getDriver*(): string =
+  let confPath = getCurrentDir() & "/config/database.ini"
+  let conf = loadConfig(confPath)
+  let driver = conf.getSectionValue("Connection", "driver")
+  return driver
+
+proc getCharset*(): string =
+  let driver = getDriver()
+  if driver == "sqlite":
+    result = ""
+  elif driver == "mysql":
+    result = &"DEFAULT CHARSET=utf8mb4"
+  elif driver == "postgres":
+    result = ""
+
+proc serialGenerator*(name: string): string =
+  let driver = getDriver()
+  if driver == "sqlite":
+    result = &"{name} INTEGER PRIMARY KEY"
+  elif driver == "mysql":
+    result = &"{name} BIGMINT NOT NULL AUTO_INCREMENT"
+  elif driver == "postgres":
+    result = &"{name} serial PRIMARY KEY"
+
+proc intGenerator*(name: string, notNull: bool): string =
+  let driver = getDriver()
+  if driver == "sqlite":
+    result = &"{name} INTEGER"
+
+  if notNull:
+    result.add(" NOT NULL")
