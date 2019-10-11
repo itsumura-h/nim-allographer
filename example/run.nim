0951272b6d87008381cdf1591c143bda32fab5b2
itsumura-h <dumblepy@gmail.com>
2019-10-11 18:02:23 +0900

    wip

--- example/run.nim
+++ example/run.nim
@@ -27,30 +27,30 @@ echo RDB().table("users").select("id", "email").limit(5).find(3, db)
 
 echo ""
 
-RDB().table("users").insert(%*{"name": "John", "email": "John@gmail.com"}).exec(db)
-echo RDB().table("users").insert(%*{"name": "John", "email": "John@gmail.com"}).execID(db)
+# RDB().table("users").insert(%*{"name": "John", "email": "John@gmail.com"}).exec(db)
+# echo RDB().table("users").insert(%*{"name": "John", "email": "John@gmail.com"}).execID(db)
 
-RDB().table("users").insert(
-    [
-        %*{"name": "John", "email": "John@gmail.com"},
-        %*{"name": "Paul", "email": "Paul@gmail.com"}
-    ]
-)
-.exec(db)
-RDB().table("users").inserts(
-    [
-        %*{"name": "John", "email": "John@gmail.com"},
-        %*{"name": "Paul", "password": "PaulPass"}
-    ]
-)
-.exec(db)
+# RDB().table("users").insert(
+#     [
+#         %*{"name": "John", "email": "John@gmail.com"},
+#         %*{"name": "Paul", "email": "Paul@gmail.com"}
+#     ]
+# )
+# .exec(db)
+# RDB().table("users").inserts(
+#     [
+#         %*{"name": "John", "email": "John@gmail.com"},
+#         %*{"name": "Paul", "password": "PaulPass"}
+#     ]
+# )
+# .exec(db)
 
 
-RDB().table("users").where("id", "=", 2).update(%*{"name": "David"}).exec(db)
-echo RDB().table("users").where("id", "=", 2).update(%*{"name": "David"}).execID(db)
-echo RDB().table("users").select().where("name", "=", "David").get(db)
+# RDB().table("users").where("id", "=", 2).update(%*{"name": "David"}).exec(db)
+# echo RDB().table("users").where("id", "=", 2).update(%*{"name": "David"}).execID(db)
+# echo RDB().table("users").select().where("name", "=", "David").get(db)
 
-echo ""
+# echo ""
 
-RDB().table("users").where("name", "=", "David").delete().exec(db)
-RDB().table("users").delete(3).exec(db)
+# RDB().table("users").where("name", "=", "David").delete().exec(db)
+# RDB().table("users").delete(3).exec(db)
