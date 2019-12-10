97e58d3218cfdf13e30a2f2ce62ec40eb78763c0
itsumura-h <dumblepy@gmail.com>
2019-12-10 18:26:37 +0900

    wip

--- example/typedReturn.nim
+++ example/typedReturn.nim
@@ -1,25 +1,35 @@
-import times
+import times, json, typeinfo, macros
 
-type Type = ref object
-  id: int
-  name: string
-  birth_date: DateTime
+# type Type = ref object
+#   id: int
+#   name: string
+#   birth_date: DateTime
 
-let response = Type(id:1, name:"John", birth_date: "1990-01-01".parse("yyyy-MM-dd"))
-echo repr response
+# let response = Type(id:1, name:"John", birth_date: "1990-01-01".parse("yyyy-MM-dd"))
+var typ: tuple[id:int, name:string, birth_date:DateTime]
+var response = @[
+                  %*{"id": 1, "name":"user1", "birth_date":"1990-01-01"},
+                  %*{"id": 2, "name":"user2", "birth_date":"1990-01-02"}
+                ]
 
-echo response.id
-echo response.id.type
+macro orm(head, body: untyped) =
+  echo head
+  echo body
 
-echo response.name
-echo response.name.type
+orm typ:
+ response
+# for v in typ.fields:
+#   echo v
 
-echo response.birth_date
-echo response.birth_date.type
+#[
 
-response.id = 2
-echo repr response
+proc orm(response_arg:openArray[JsonNode], typ:var tuple):seq[typ.type] =
+  var response: seq[typ.type]
+  for row in response_arg:
+    typ.id = row["id"].getInt()
+    typ.name = row["name"].getStr()
+    typ.birth_date = row["birth_date"].getStr().parse("yyyy-MM-dd")
+    response.add(typ)
+  return response
 
-let columnName = "name"
-
-# echo response.getter(columnName)
+]#
