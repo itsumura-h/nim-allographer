ee95f13e386902b7f7634605a36ead851613be8d
itsumura-h <dumblepy@gmail.com>
2019-12-11 14:52:27 +0900

    wip #18 try to create macro

--- tests/test_query_return_type.nim
+++ tests/test_query_return_type.nim
@@ -1,3 +1,4 @@
+from macros import parseStmt
 import times, strformat, json
 
 import ../src/allographer/query_builder
@@ -24,9 +25,21 @@ for i in 1..5:
 RDB().table("users").insert(users).exec()
 
 
-template orm(head, body: untyped) =
-  echo head
-  echo body
+macro orm(head, body: untyped): untyped =
+  var strBody = ""
+  strBody.add(fmt"""
+proc toTuple(response_arg:openarray[JsonNode], typ:tuple):seq[]
+  var response: seq[typ.type]
+  for row in response_arg:
+    typ.id = row["id"].getInt()
+    typ.name = row["name"].getStr()
+    typ.birth_date = row["birth_date"].getStr().parse("yyyy-MM-dd")
+    response.add(typ)
+  return response
+
+return toTuple({repr head}, {repr body})""")
+  result = parseStmt(strBody)
+
 
   # var response: seq[body.type]
   # for row in head:
@@ -38,6 +51,7 @@ template orm(head, body: untyped) =
 
 
 var typ: tuple[id:int, name:string, birth_date:DateTime]
-var response = RDB().table("users").get()
-echo response
-echo response[0]["id"]
+RDB().table("users").get().orm(typ)
+# var response = RDB().table("users").get().orm(typ)
+# echo response
+# echo response[0]["id"]
