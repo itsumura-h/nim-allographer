de30a7ddea1722aad1b88ba15a4574ae7bca9e8b
itsumura-h <dumblepy@gmail.com>
2019-12-16 13:45:55 +0900

    closed #18 complete

--- tests/test_query_return_type.nim
+++ tests/test_query_return_type.nim
@@ -1,14 +1,16 @@
-import macros, times, strformat, json, strutils, re, options
+import unittest, strformat, json, strutils
 
 import ../src/allographer/query_builder
 import ../src/allographer/schema_builder
+
 
 Schema().create(
   Table().create("users", [
     Column().increments("id"),
     COlumn().string("name").nullable(),
     Column().date("birth_date").nullable(),
-    Column().string("null").nullable()
+    Column().string("null").nullable(),
+    Column().boolean("bool").default(false)
   ], reset=true)
 )
 
@@ -24,93 +26,45 @@ for i in 1..5:
 RDB().table("users").insert(users).exec()
 
 
-proc ormProc(response_arg:seq[seq[string]], typ:var tuple, responseName:string) =
-  var response: seq[typ.type]
-  var responseRaw = newNimNode(nnkTupleTy)
-  echo repr responseRaw
-  for row in response_arg:
-    for typRow in typ.fields:
-      echo "-----------------------"
-      # echo row[i]
-      echo typRow
-      echo typRow.type
-    response.add(typ)
-  echo response
+type Typ = ref object
+  id:int
+  name:string
+  birth_date:string
+  null:string
+  bool:bool
 
+suite "return with type":
+  test "get":
+    var t = Typ(id:1, name:"user1", birth_date:"1990-01-01", null:"")
+    var r = RDB().table("users").get(Typ())[0]
+    check t.id == r.id
+    check t.name == r.name
+    check t.birth_date == r.birth_date
+    check t.null == r.null
+    check t.bool == r.bool
+  test "getRaw":
+    var t = Typ(id:1, name:"user1", birth_date:"1990-01-01", null:"")
+    var r = RDB().raw("select * from users").getRaw(Typ())[0]
+    check t.id == r.id
+    check t.name == r.name
+    check t.birth_date == r.birth_date
+    check t.null == r.null
+    check t.bool == r.bool
+  test "first":
+    var t = Typ(id:1, name:"user1", birth_date:"1990-01-01", null:"")
+    var r = RDB().table("users").first(Typ())
+    check t.id == r.id
+    check t.name == r.name
+    check t.birth_date == r.birth_date
+    check t.null == r.null
+    check t.bool == r.bool
+  test "find":
+    var t = Typ(id:1, name:"user1", birth_date:"1990-01-01", null:"")
+    var r = RDB().table("users").find(1, Typ())
+    check t.id == r.id
+    check t.name == r.name
+    check t.birth_date == r.birth_date
+    check t.null == r.null
+    check t.bool == r.bool
 
-macro ormMacro(response_arg, typ, responseName:typed):untyped =
-  echo typ.getTypeInst.repr.type
-  var typKeys: seq[string]
-  for m in typ.getTypeInst.repr.split({'[', ' ', ']'}):
-    if m.contains(":"):
-      var typKey = m.split(":")[0] 
-      typKeys.add(typKey)
-  echo typKeys
-
-
-  result = parseStmt("")
-  for i, typKey in typKeys:
-    result.add(parseStmt(fmt"""
-echo {typ.repr}.{typKey}, "    ", {i}
-"""))
-
-  result.add(parseStmt(fmt"""
-for i, row in {response_arg.repr}:
-  echo "-------------------"
-  for i, column in row:
-    echo i, "     ", column
-"""))
-
-
-#   var strBody = fmt"""
-# var {responseName}: seq[{repr typ}.type]
-# for i, typKey in {typKeys}.pairs:
-#   echo i, "  ", typKey
-#   if {repr typ}[i].type == int:
-#     {repr typ}.{typKey} = row[i].parseInt()
-#   elif {repr typ}[i].tpye == "".type:
-#     {repr typ}.{typKey} = row[i]
-#   elif {repr typ}[i].tpye == Datetime:
-#     {repr typ}.{typKey} = row[i].parse("yyyy-MM-dd")
-# {responseName}.add(typ)"""
-
-#   result = parseStmt(strBody)
-
-
-var typ: tuple[id:int, name:string, birth_date:DateTime]
-# RDB().table("users").getString().ormProc(typ, "response")
-RDB().table("users").getString().ormMacro(typ, "response")
-# echo response
-
-
-#[
-
-if {repr typ}[i].type == int:
-  {repr typ}.{typKey} = row[i].parseInt()
-elif {repr typ}[i].tpye == "".type:
-  {repr typ}.{typKey} = row[i]
-elif {repr typ}[i].tpye == Datetime:
-  {repr typ}.{typKey} = row[i].parse("yyyy-MM-dd")
-
-macro orm(response_arg, typ, responseName: untyped): untyped =
-  var strBody = fmt"""
-var {responseName}: seq[{repr typ}.type]
-for i, row in {repr response_arg}.pairs:
-  {repr typ}.id = row["id"].getInt()
-  {repr typ}.name = row["name"].getStr()
-  {repr typ}.birth_date = row["birth_date"].getStr().parse("yyyy-MM-dd")
-  {responseName}.add(typ)"""
-
-  result = parseStmt(strBody)
-
-
-
-var response: seq[body.type]
-for row in head:
-  body.id = row["id"].getInt()
-  body.name = row["name"].getStr()
-  body.birth_date = row["birth_date"].getStr().parse("yyyy-MM-dd")
-  response.add(typ)
-response
-
-]#
+RDB().raw("DROP TABLE users").exec()
