e39432964961a0adede7ee1b25912244d34ff14e
itsumura-h <dumblepy@gmail.com>
2019-12-29 23:20:09 +0900

    uodate #34
    standardize sql generate process
    fix mysql return not tinyint but bool

--- src/allographer/schema_builder/generators/mysql_generators.nim
+++ src/allographer/schema_builder/generators/mysql_generators.nim
@@ -5,7 +5,7 @@ import ../column
 # int
 # =============================================================================
 proc serialGenerator*(name:string):string =
-  result = &"`{name}` INT NOT NULL PRIMARY KEY AUTO_INCREMENT"
+  result = &"`{name}` BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT"
 
 proc intGenerator*(name:string, nullable:bool, isDefault:bool, default:int,
                     isUnsigned:bool):string =
@@ -207,7 +207,8 @@ proc blobGenerator*(name:string, nullable:bool):string =
 
 proc boolGenerator*(name:string, nullable:bool, isDefault:bool, 
                     default:bool):string =
-  result = &"`{name}` TINYINT"
+  # result = &"`{name}` TINYINT(1)"
+  result = &"`{name}` BOOLEAN"
 
   if isDefault:
     let defaultInt = if default: 1 else: 0
