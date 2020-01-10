4d135931ae652fa5f785d401b39bf86ff5aede63
itsumura-h <dumblepy@gmail.com>
2020-01-10 17:55:15 +0900

    wip not use conf file

--- src/allographer/connection.nim
+++ src/allographer/connection.nim
@@ -4,6 +4,18 @@ proc db*(): DbConn =
   open("/home/www/db.sqlite3", "user", "Password!", "allographer")
 
 const DRIVER = "sqlite"
-
 proc getDriver*():string =
   return DRIVER
+
+
+const CONSOLE_LOG = true
+proc getConsoleLog*():bool =
+  return CONSOLE_LOG
+
+const FILE_LOG = true
+proc getFileLog*():bool =
+  return FILE_LOG
+
+const LOG_DIR = "/home/www/logs"
+proc getLogDir*():string =
+  return LOG_DIR
