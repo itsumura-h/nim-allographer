0f4dd8cefaed1abfb38e81d8416619262eb698b7
itsumura-h <dumblepy@gmail.com>
2020-03-25 02:21:48 +0900

    wip

--- src/allographer/query_builder/transaction.nim
+++ src/allographer/query_builder/transaction.nim
@@ -1,20 +1,21 @@
 import macros, strutils, strformat
 import ../connection, base
 
-proc addDb(bobyStr:string):string =
-  return bobyStr.replace("RDB()", "RDB(db:db)")
 
 macro transaction*(bodyInput: untyped):untyped =
-  var bodyStr = bodyInput.repr.addDb()
-  var body = parseStmt(fmt"""
-let db = db()
+  var bodyStr = bodyInput.repr
+                .replace("RDB()", "RDB(db:db)")
+                .indent(2)
+  bodyStr = fmt"""
+let db = query_builder.db()
 defer: db.close()
 try:
   db.exec(sql"BEGIN")
-  {bodyStr}
+{bodyStr}
+  db.exec(sql"COMMIT")
 except:
   db.exec(sql"ROLLBACK")
-db.exec(sql"COMMIT")
-""")
-  echo body
+"""
+  echo bodyStr
+  let body = bodyStr.parseStmt()
   return body
