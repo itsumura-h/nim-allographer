3240289dbdbf765c18b63a647e6840efcea052e6
itsumura-h <dumblepy@gmail.com>
2020-03-25 18:08:03 +0900

    cannot close db

--- src/allographer/query_builder/transaction.nim
+++ src/allographer/query_builder/transaction.nim
@@ -3,18 +3,23 @@ import ../connection, base
 
 
 macro transaction*(bodyInput: untyped):untyped =
-  var bodyStr = bodyInput.repr
-                .replace("RDB()", "RDB(db:db)")
-                .indent(2)
+  var bodyStr = bodyInput.repr.replace("RDB()", "RDB(db:db)")
+  bodyStr.removePrefix
+  bodyStr = bodyStr.indent(2)
   bodyStr = fmt"""
 let db = query_builder.db()
-defer: db.close()
+# defer: db.close()
+db.exec(sql"BEGIN")
 try:
-  db.exec(sql"BEGIN")
 {bodyStr}
   db.exec(sql"COMMIT")
 except:
+  echo "=== rollback"
   db.exec(sql"ROLLBACK")
+finally:
+  echo "=== finished"
+  echo db.repr
+  db.close()
 """
   echo bodyStr
   let body = bodyStr.parseStmt()
