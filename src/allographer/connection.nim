02853190fed98f9473f905c240f937ecd41e647e
itsumura-h <dumblepy@gmail.com>
2021-04-11 11:46:31 +0000

    fix DB_CONNECTION default val

--- src/allographer/connection.nim
+++ src/allographer/connection.nim
@@ -9,7 +9,7 @@ if (getCurrentDir() / ".env").fileExists:
   env.load()
 
 let
-  CONN = getEnv("DB_CONNECTION", "sqlite").string
+  CONN = getEnv("DB_CONNECTION", getCurrentDir() / "db.sqlite3").string
   USER = getEnv("DB_USER", "").string
   PASSWORD = getEnv("DB_PASSWORD", "").string
   DATABASE = getEnv("DB_DATABASE", "").string
