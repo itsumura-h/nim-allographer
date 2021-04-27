1379397c158fa6c3dc19ef2b046f769b5286c428
itsumra-h <dumblepy@gmail.com>
2021-04-27 20:20:44 +0900

    fix env boilerplate

--- permission.sh
+++ permission.sh
@@ -4,5 +4,5 @@ local_GID=$(id -g $USER)
 echo 'start'
 sudo chown ${local_UID}:${local_GID} * -R
 echo '======================'
-sudo find . -name ".*" -print | xargs sudo chown ${local_UID}:${local_GID}
+sudo chown ${local_UID}:${local_GID} .* -R
 echo 'end'
