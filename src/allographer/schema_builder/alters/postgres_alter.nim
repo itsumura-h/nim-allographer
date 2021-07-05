329d5bde73addb56c124d01ebadbb9f6b6148483
itsumra-h <dumblepy@gmail.com>
2021-07-05 00:20:24 +0000

    add try catch in alter table

--- src/allographer/schema_builder/alters/postgres_alter.nim
+++ src/allographer/schema_builder/alters/postgres_alter.nim
@@ -27,63 +27,79 @@ proc add(column:Column, table:string) =
 proc change(column:Column, table:string) =
   let db = db()
   defer: db.close()
-  var columnString = generateColumnString(column)
-  let columnTyp = columnString.split(" ")[1]
-  # change column difinition
-  var query = &"ALTER TABLE {table} ALTER COLUMN {column.previousName} TYPE {columnTyp}"
-  logger(query)
-  db.exec(sql query)
-  # change column name
-  query = &"ALTER TABLE {table} RENAME COLUMN {column.previousName} TO {column.name}"
-  logger(query)
-  db.exec(sql query)
-  # delete option
-  query = &"ALTER TABLE {table} ALTER {column.name} DROP DEFAULT"
-  logger(query)
-  db.exec(sql query)
-  query = &"ALTER TABLE {table} ALTER {column.name} DROP NOT NULL"
-  logger(query)
-  db.exec(sql query)
-  query = &"ALTER TABLE {table} DROP CONSTRAINT {table}_{column.previousName}"
-  logger(query)
-  db.exec(sql query)
-  # set options
-  if columnString.contains("DEFAULT"):
-    let regex = """DEFAULT\s('.*'|\d)"""
-    let defautSetting = findAll(columnString, re(regex))[0]
-    query = &"ALTER TABLE {table} ALTER {column.name} SET {defautSetting}"
+  try:
+    var columnString = generateColumnString(column)
+    let columnTyp = columnString.split(" ")[1]
+    # change column difinition
+    var query = &"ALTER TABLE {table} ALTER COLUMN {column.previousName} TYPE {columnTyp}"
     logger(query)
     db.exec(sql query)
-  if columnString.contains("NOT NULL"):
-    query = &"ALTER TABLE {table} ALTER {column.name} SET NOT NULL"
-    db.exec(sql query)
-  if columnString.contains("CHECK"):
-    let regex = "CHECK \\(.+?\\)"
-    let checkSetting = findAll(columnString, re(regex))[0]
-    query = &"ALTER TABLE {table} ADD CONSTRAINT {table}_{column.name} {checkSetting}"
+    # change column name
+    query = &"ALTER TABLE {table} RENAME COLUMN {column.previousName} TO {column.name}"
     logger(query)
     db.exec(sql query)
+    # delete option
+    query = &"ALTER TABLE {table} ALTER {column.name} DROP DEFAULT"
+    logger(query)
+    db.exec(sql query)
+    query = &"ALTER TABLE {table} ALTER {column.name} DROP NOT NULL"
+    logger(query)
+    db.exec(sql query)
+    query = &"ALTER TABLE {table} DROP CONSTRAINT {table}_{column.previousName}"
+    logger(query)
+    db.exec(sql query)
+    # set options
+    if columnString.contains("DEFAULT"):
+      let regex = """DEFAULT\s('.*'|\d)"""
+      let defautSetting = findAll(columnString, re(regex))[0]
+      query = &"ALTER TABLE {table} ALTER {column.name} SET {defautSetting}"
+      logger(query)
+      db.exec(sql query)
+    if columnString.contains("NOT NULL"):
+      query = &"ALTER TABLE {table} ALTER {column.name} SET NOT NULL"
+      db.exec(sql query)
+    if columnString.contains("CHECK"):
+      let regex = "CHECK \\(.+?\\)"
+      let checkSetting = findAll(columnString, re(regex))[0]
+      query = &"ALTER TABLE {table} ADD CONSTRAINT {table}_{column.name} {checkSetting}"
+      logger(query)
+      db.exec(sql query)
+  except:
+    let err = getCurrentExceptionMsg()
+    echoErrorMsg(err)
 
 proc delete(column:Column, table:string) =
   let db = db()
   defer: db.close()
-  let query = &"ALTER TABLE {table} DROP {column.previousName}"
-  logger(query)
-  db.exec(sql query)
+  try:
+    let query = &"ALTER TABLE {table} DROP {column.previousName}"
+    logger(query)
+    db.exec(sql query)
+  except:
+    let err = getCurrentExceptionMsg()
+    echoErrorMsg(err)
 
 proc rename(tableFrom, tableTo:string) =
   let db = db()
   defer: db.close()
-  let query = &"ALTER TABLE {tableFrom} RENAME TO {tableTo}"
-  logger(query)
-  db.exec(sql query)
+  try:
+    let query = &"ALTER TABLE {tableFrom} RENAME TO {tableTo}"
+    logger(query)
+    db.exec(sql query)
+  except:
+    let err = getCurrentExceptionMsg()
+    echoErrorMsg(err)
 
 proc drop(table:string) =
   let db = db()
   defer: db.close()
-  let query = &"DROP TABLE {table}"
-  logger(query)
-  db.exec(sql query)
+  try:
+    let query = &"DROP TABLE {table}"
+    logger(query)
+    db.exec(sql query)
+  except:
+    let err = getCurrentExceptionMsg()
+    echoErrorMsg(err)
 
 proc exec*(table:Table) =
   if table.typ == Nomal:
