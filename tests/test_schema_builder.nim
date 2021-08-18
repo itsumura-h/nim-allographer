d70825f74e1758ef489db4c87a47f5879081d911
itsumra-h <dumblepy@gmail.com>
2021-08-18 11:48:56 +0000

    .

--- tests/test_schema_builder.nim
+++ tests/test_schema_builder.nim
@@ -5,45 +5,45 @@ import ../src/allographer/connection
 
 let maxConnections = getEnv("DB_MAX_CONNECTION").parseInt
 
-# let sqliteDb = dbopen(SQLite3, ":memory:", maxConnections=95, timeout=30, shouldDisplayLog=true)
-# let postgresDb = dbopen(PostgreSQL, getEnv("DB_DATABASE"), getEnv("DB_USER"), getEnv("DB_PASSWORD"), getEnv("PG_HOST"), getEnv("PG_PORT").parseInt, maxConnections, 30, shouldDisplayLog=true)
+let sqliteDb = dbopen(SQLite3, ":memory:", maxConnections=95, timeout=30, shouldDisplayLog=true)
+let postgresDb = dbopen(PostgreSQL, getEnv("DB_DATABASE"), getEnv("DB_USER"), getEnv("DB_PASSWORD"), getEnv("PG_HOST"), getEnv("PG_PORT").parseInt, maxConnections, 30, shouldDisplayLog=true)
 # let mysqlDb = dbopen(MySQL, getEnv("DB_DATABASE"), getEnv("DB_USER"), getEnv("DB_PASSWORD"), getEnv("MY_HOST"), getEnv("MY_PORT").parseInt, maxConnections, 30, shouldDisplayLog=true)
-let mysqlDb = dbopen(MariaDB, getEnv("DB_DATABASE"), getEnv("DB_USER"), getEnv("DB_PASSWORD"), getEnv("MARIA_HOST"), getEnv("MY_PORT").parseInt, maxConnections, 30, shouldDisplayLog=true)
+let mariaDb = dbopen(MariaDB, getEnv("DB_DATABASE"), getEnv("DB_USER"), getEnv("DB_PASSWORD"), getEnv("MARIA_HOST"), getEnv("MY_PORT").parseInt, maxConnections, 30, shouldDisplayLog=true)
 
 suite "Schema builder":
   test "test":
-    # sqliteDb.schema(
-    #   table("schema_builder", [
-    #     Column().increments("increments_column"),
-    #     Column().integer("integer_column").unique().default(1).unsigned().index(),
-    #     Column().smallInteger("smallInteger_column").unique().default(1).unsigned().index(),
-    #     Column().mediumInteger("mediumInteger_column").unique().default(1).unsigned().index(),
-    #     Column().bigInteger("bigInteger_column").unique().default(1).unsigned().index(),
+    sqliteDb.schema(
+      table("schema_builder", [
+        Column().increments("increments_column"),
+        Column().integer("integer_column").unique().default(1).unsigned().index(),
+        Column().smallInteger("smallInteger_column").unique().default(1).unsigned().index(),
+        Column().mediumInteger("mediumInteger_column").unique().default(1).unsigned().index(),
+        Column().bigInteger("bigInteger_column").unique().default(1).unsigned().index(),
 
-    #     Column().decimal("decimal_column", 5, 2).unique().default(1).unsigned().index(),
-    #     Column().double("double_column", 5, 2).unique().default(1).unsigned().index(),
-    #     Column().float("float_column").unique().default(1).unsigned().index(),
+        Column().decimal("decimal_column", 5, 2).unique().default(1).unsigned().index(),
+        Column().double("double_column", 5, 2).unique().default(1).unsigned().index(),
+        Column().float("float_column").unique().default(1).unsigned().index(),
 
-    #     Column().char("char_column", 100).unique().default("").unsigned().index(),
-    #     Column().string("string_column").unique().default("").unsigned().index(),
-    #     Column().text("text_column").unique().default("").unsigned().index(),
-    #     Column().mediumText("mediumText_column").unique().default("").unsigned().index(),
-    #     Column().longText("longText_column").unique().default("").unsigned().index(),
+        Column().char("char_column", 100).unique().default("").unsigned().index(),
+        Column().string("string_column").unique().default("").unsigned().index(),
+        Column().text("text_column").unique().default("").unsigned().index(),
+        Column().mediumText("mediumText_column").unique().default("").unsigned().index(),
+        Column().longText("longText_column").unique().default("").unsigned().index(),
 
-    #     Column().date("date_column").unique().default().unsigned().index(),
-    #     Column().datetime("datetime_column").unique().default().unsigned().index(),
-    #     Column().time("time_column").unique().default().unsigned().index(),
-    #     Column().timestamp("timestamp_column").unique().default().unsigned().index(),
-    #     Column().timestamps(),
-    #     Column().softDelete(),
+        Column().date("date_column").unique().default().unsigned().index(),
+        Column().datetime("datetime_column").unique().default().unsigned().index(),
+        Column().time("time_column").unique().default().unsigned().index(),
+        Column().timestamp("timestamp_column").unique().default().unsigned().index(),
+        Column().timestamps(),
+        Column().softDelete(),
 
-    #     Column().binary("binary_column").unique().default().unsigned().index(),
-    #     Column().boolean("boolean_column").unique().default().index(),
-    #     Column().enumField("enumField_column", ["a", "b"]).unique().default().index(),
-    #     Column().json("json_column").unique().default(%*{"key": "value"}).unsigned().index(),
-    #   ], reset=true)
-    # )
-    mysqlDb.schema(
+        Column().binary("binary_column").unique().default().unsigned().index(),
+        Column().boolean("boolean_column").unique().default().index(),
+        Column().enumField("enumField_column", ["a", "b"]).unique().default().index(),
+        Column().json("json_column").unique().default(%*{"key": "value"}).unsigned().index(),
+      ], reset=true)
+    )
+    mariaDb.schema(
       table("schema_builder", [
         Column().increments("increments_column"),
         Column().integer("integer_column").unique().default(1).unsigned(),
@@ -74,43 +74,42 @@ suite "Schema builder":
         Column().json("json_column"),
       ], reset=true)
     )
-    # postgresDb.schema(
-    #   table("schema_builder", [
-    #     Column().increments("increments_column"),
-    #     Column().integer("integer_column").unique().default(1).unsigned(),
-    #     Column().smallInteger("smallInteger_column").unique().default(1).unsigned(),
-    #     Column().mediumInteger("mediumInteger_column").unique().default(1).unsigned(),
-    #     Column().bigInteger("bigInteger_column").unique().default(1).unsigned(),
+    postgresDb.schema(
+      table("schema_builder", [
+        Column().increments("increments_column"),
+        Column().integer("integer_column").unique().default(1).unsigned(),
+        Column().smallInteger("smallInteger_column").unique().default(1).unsigned(),
+        Column().mediumInteger("mediumInteger_column").unique().default(1).unsigned(),
+        Column().bigInteger("bigInteger_column").unique().default(1).unsigned(),
 
-    #     Column().decimal("decimal_column", 5, 2).unique().default(1).unsigned(),
-    #     Column().double("double_column", 5, 2).unique().default(1).unsigned(),
-    #     Column().float("float_column").unique().default(1).unsigned(),
+        Column().decimal("decimal_column", 5, 2).unique().default(1).unsigned(),
+        Column().double("double_column", 5, 2).unique().default(1).unsigned(),
+        Column().float("float_column").unique().default(1).unsigned(),
 
-    #     Column().char("char_column", 100).unique().default(""),
-    #     Column().string("string_column").unique().default(""),
-    #     Column().text("text_column").unique().default(""),
-    #     Column().mediumText("mediumText_column").unique().default(""),
-    #     Column().longText("longText_column").unique().default(""),
+        Column().char("char_column", 100).unique().default(""),
+        Column().string("string_column").unique().default(""),
+        Column().text("text_column").unique().default(""),
+        Column().mediumText("mediumText_column").unique().default(""),
+        Column().longText("longText_column").unique().default(""),
 
-    #     Column().date("date_column").unique().default(),
-    #     Column().datetime("datetime_column").unique().default(),
-    #     Column().time("time_column").unique().default(),
-    #     Column().timestamp("timestamp_column").unique().default(),
-    #     Column().timestamps(),
-    #     Column().softDelete(),
+        Column().date("date_column").unique().default(),
+        Column().datetime("datetime_column").unique().default(),
+        Column().time("time_column").unique().default(),
+        Column().timestamp("timestamp_column").unique().default(),
+        Column().timestamps(),
+        Column().softDelete(),
 
-    #     Column().binary("binary_column").unique().default(),
-    #     Column().boolean("boolean_column").unique().default(),
-    #     Column().enumField("enumField_column", ["a", "b"]).unique().default(),
-    #     Column().json("json_column").default(%*{"key": "value"}),
-    #   ], reset=true)
-    # )
+        Column().binary("binary_column").unique().default(),
+        Column().boolean("boolean_column").unique().default(),
+        Column().enumField("enumField_column", ["a", "b"]).unique().default(),
+        Column().json("json_column").default(%*{"key": "value"}),
+      ], reset=true)
+    )
 
   test "insert":
     waitFor (proc(){.async.}=
       try:
-        # for rdb in [sqliteDb, mysqlDb, postgresDb]:
-        for rdb in [mysqlDb]:
+        for rdb in [sqliteDb, mariaDb, postgresDb]:
           await rdb.table("schema_builder").insert(%*{
             "increments_column": 1,
             "integer_column": 1,
