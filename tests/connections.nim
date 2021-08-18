5b04a7ee2e26ba9f5ea329bdfdcbb85ec5a51eb1
itsumra-h <dumblepy@gmail.com>
2021-08-18 12:13:01 +0000

    .

--- tests/connections.nim
+++ tests/connections.nim
@@ -16,7 +16,7 @@ let
 
 let
   sqliteDb = dbopen(SQLite3, sqliteHost, maxConnections=maxConnections)
-  mysqlDb = dbopen(MySQL, database, user, password, mysqlHost, mysqlPort, maxConnections, timeout)
+  # mysqlDb = dbopen(MySQL, database, user, password, mysqlHost, mysqlPort, maxConnections, timeout)
   mariaDb = dbopen(MariaDB, database, user, password, mysqlHost, mysqlPort, maxConnections, timeout)
   postgresDb = dbopen(PostgreSQL, database, user, password, pgHost, pgPort, maxConnections, timeout)
   rdb* = sqliteDb
