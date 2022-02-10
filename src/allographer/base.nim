bf899e6d9fa0e9a0325e0d4208f09eee7d209ff8
itsumura-h <39766805+itsumura-h@users.noreply.github.com>
2022-02-10 14:37:47 +0900

    remove dotenv (#188)
    close #186

--- src/allographer/base.nim
+++ src/allographer/base.nim
@@ -1,12 +1,18 @@
-import json
+import os, strutils, json, streams, parsecfg
 import async/async_db
-
-import os, strutils
-import dotenv
 
 for f in walkDir(getCurrentDir()):
   if f.path.split("/")[^1] == ".env":
-    load(getCurrentDir(), ".env")
+    let path = getCurrentDir() / ".env"
+    var f = newFileStream(path, fmRead)
+    var p: CfgParser
+    open(p, f, path)
+    while true:
+      let e = next(p)
+      case e.kind
+      of cfgEof: break
+      of cfgKeyValuePair: putEnv(e.key, e.value)
+      else: discard
     break
 
 type
