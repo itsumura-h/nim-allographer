0146b6d4119be5bea71de2561f05b5adb373e938
itsumra-h <dumblepy@gmail.com>
2022-05-08 06:31:34 +0000

    wip

--- tests/connections.nim
+++ tests/connections.nim
@@ -1,15 +1,12 @@
 import
   std/os,
   std/strutils,
-  std/distros,
   ../src/allographer/connection
 
 let
   database = getEnv("DB_DATABASE")
   user = getEnv("DB_USER")
   password = getEnv("DB_PASSWORD")
-  sqliteHost = getEnv("SQLITE_HOST")
-  mysqlHost = getEnv("MY_HOST")
   mariadbHost = getEnv("MARIA_HOST")
   mysqlPort = getEnv("MY_PORT").parseInt
   pgHost = getEnv("PG_HOST")
@@ -21,9 +18,9 @@ let
   rdb* = dbopen(SQLite3, ":memory:", maxConnections=maxConnections, shouldDisplayLog=true)
 
 let dbConnections* = @[
-  dbopen(SQLite3, ":memory:", maxConnections=95, timeout=30, shouldDisplayLog=false),
-  dbopen(PostgreSQL, database, user, password, pgHost, pgPort, maxConnections, 30, shouldDisplayLog=false),
-  dbopen(MariaDB, database, user, password, mariadbHost, mysqlPort, maxConnections, 30, shouldDisplayLog=false),
+  dbopen(SQLite3, ":memory:", maxConnections=95, timeout=timeout, shouldDisplayLog=false),
+  dbopen(PostgreSQL, database, user, password, pgHost, pgPort, maxConnections, timeout, shouldDisplayLog=false),
+  dbopen(MariaDB, database, user, password, mariadbHost, mysqlPort, maxConnections, timeout, shouldDisplayLog=false),
 ]
 
 template asyncBlock*(body:untyped) =
