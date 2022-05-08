e69acd3624d30771986e32aac9848c279804f5a9
itsumura-h <39766805+itsumura-h@users.noreply.github.com>
2022-05-08 20:32:00 +0900

    fix building sql proccess (#196)
    close #144

--- src/allographer/query_builder/builders.nim
+++ src/allographer/query_builder/builders.nim
@@ -6,107 +6,124 @@ import
 
 # ==================== SELECT ====================
 
-proc selectBuilder*(self: Rdb): Rdb =
-  return self.selectSql()
-        .fromSql()
-        .joinSql()
-        .leftJoinSql()
-        .whereSql()
-        .orWhereSql()
-        .whereBetweenSql()
-        .whereBetweenStringSql()
-        .whereNotBetweenSql()
-        .whereNotBetweenStringSql()
-        .whereInSql()
-        .whereNotInSql()
-        .whereNullSql()
-        .groupBySql()
-        .havingSql()
-        .orderBySql()
-        .limitSql()
-        .offsetSql()
+proc selectBuilder*(self: Rdb): string =
+  return self
+    .selectSql()
+    .fromSql()
+    .joinSql()
+    .leftJoinSql()
+    .whereSql()
+    .orWhereSql()
+    .whereBetweenSql()
+    .whereBetweenStringSql()
+    .whereNotBetweenSql()
+    .whereNotBetweenStringSql()
+    .whereInSql()
+    .whereNotInSql()
+    .whereNullSql()
+    .groupBySql()
+    .havingSql()
+    .orderBySql()
+    .limitSql()
+    .offsetSql()
+    .sqlString
 
-proc selectFirstBuilder*(self: Rdb): Rdb =
-  return self.selectSql()
-        .fromSql()
-        .joinSql()
-        .leftJoinSql()
-        .whereSql()
-        .orWhereSql()
-        .whereBetweenSql()
-        .whereBetweenStringSql()
-        .whereNotBetweenSql()
-        .whereNotBetweenStringSql()
-        .whereInSql()
-        .whereNotInSql()
-        .whereNullSql()
-        .groupBySql()
-        .havingSql()
-        .orderBySql()
-        .selectFirstSql()
-        .offsetSql()
+proc selectFirstBuilder*(self: Rdb): string =
+  return self
+    .selectSql()
+    .fromSql()
+    .joinSql()
+    .leftJoinSql()
+    .whereSql()
+    .orWhereSql()
+    .whereBetweenSql()
+    .whereBetweenStringSql()
+    .whereNotBetweenSql()
+    .whereNotBetweenStringSql()
+    .whereInSql()
+    .whereNotInSql()
+    .whereNullSql()
+    .groupBySql()
+    .havingSql()
+    .orderBySql()
+    .selectFirstSql()
+    .offsetSql()
+    .sqlString
 
-proc selectFindBuilder*(self: Rdb, key: string): Rdb =
-  return self.selectSql()
-        .fromSql()
-        .joinSql()
-        .leftJoinSql()
-        .whereSql()
-        .orWhereSql()
-        .whereBetweenSql()
-        .whereBetweenStringSql()
-        .whereNotBetweenSql()
-        .whereNotBetweenStringSql()
-        .whereInSql()
-        .whereNotInSql()
-        .whereNullSql()
-        .selectByIdSql(key)
+proc selectFindBuilder*(self: Rdb, key: string): string =
+  return self
+    .selectSql()
+    .fromSql()
+    .joinSql()
+    .leftJoinSql()
+    .whereSql()
+    .orWhereSql()
+    .whereBetweenSql()
+    .whereBetweenStringSql()
+    .whereNotBetweenSql()
+    .whereNotBetweenStringSql()
+    .whereInSql()
+    .whereNotInSql()
+    .whereNullSql()
+    .selectByIdSql(key)
+    .sqlString
 
 
 # ==================== INSERT ====================
 
-proc insertValueBuilder*(self: Rdb, items: JsonNode): Rdb =
-  return self.insertSql()
-        .insertValueSql(items)
+proc insertValueBuilder*(self: Rdb, items: JsonNode): string =
+  return self
+    .insertSql()
+    .insertValueSql(items)
+    .sqlString
 
-proc insertValuesBuilder*(self: Rdb, rows: openArray[JsonNode]): Rdb =
-  return self.insertSql()
-        .insertValuesSql(rows)
+proc insertValuesBuilder*(self: Rdb, rows: openArray[JsonNode]): string =
+  return self
+    .insertSql()
+    .insertValuesSql(rows)
+    .sqlString
 
 
 # ==================== UPDATE ====================
 
-proc updateBuilder*(self: Rdb, items: JsonNode): Rdb =
-  return self.updateSql()
-        .updateValuesSql(items)
-        .whereSql()
-        .orWhereSql()
-        .limitSql()
-        .offsetSql()
+proc updateBuilder*(self: Rdb, items: JsonNode): string =
+  return self
+    .updateSql()
+    .updateValuesSql(items)
+    .whereSql()
+    .orWhereSql()
+    .limitSql()
+    .offsetSql()
+    .sqlString
 
 
 # ==================== DELETE ====================
 
-proc deleteBuilder*(self: Rdb): Rdb =
-  return self.deleteSql()
-        .fromSql()
-        .joinSql()
-        .leftJoinSql()
-        .whereSql()
-        .orWhereSql()
-        .limitSql()
-        .offsetSql()
+proc deleteBuilder*(self: Rdb): string =
+  return self
+    .deleteSql()
+    .fromSql()
+    .joinSql()
+    .leftJoinSql()
+    .whereSql()
+    .orWhereSql()
+    .limitSql()
+    .offsetSql()
+    .sqlString
 
-proc deleteByIdBuilder*(self: Rdb, id: int, key: string): Rdb =
-  return self.deleteSql()
-        .fromSql()
-        .deleteByIdSql(id, key)
+proc deleteByIdBuilder*(self: Rdb, id: int, key: string): string =
+  return self
+    .deleteSql()
+    .fromSql()
+    .deleteByIdSql(id, key)
+    .sqlString
 
 
 # ==================== Aggregates ====================
 
-proc countBuilder*(self:Rdb): Rdb =
-  return self.selectCountSql()
+proc countBuilder*(self:Rdb): string =
+  return self
+    .selectCountSql()
     .fromSql()
     .joinSql()
     .leftJoinSql()
@@ -122,9 +139,11 @@ proc countBuilder*(self:Rdb): Rdb =
     .orderBySql()
     .limitSql()
     .offsetSql()
+    .sqlString
 
-proc maxBuilder*(self:Rdb, column:string): Rdb =
-  return self.selectMaxSql(column)
+proc maxBuilder*(self:Rdb, column:string): string =
+  return self
+    .selectMaxSql(column)
     .fromSql()
     .joinSql()
     .leftJoinSql()
@@ -140,9 +159,11 @@ proc maxBuilder*(self:Rdb, column:string): Rdb =
     .orderBySql()
     .limitSql()
     .offsetSql()
+    .sqlString
 
-proc minBuilder*(self:Rdb, column:string): Rdb =
-  return self.selectMinSql(column)
+proc minBuilder*(self:Rdb, column:string): string =
+  return self
+    .selectMinSql(column)
     .fromSql()
     .joinSql()
     .leftJoinSql()
@@ -158,9 +179,11 @@ proc minBuilder*(self:Rdb, column:string): Rdb =
     .orderBySql()
     .limitSql()
     .offsetSql()
+    .sqlString
 
-proc avgBuilder*(self:Rdb, column:string): Rdb =
-  return self.selectAvgSql(column)
+proc avgBuilder*(self:Rdb, column:string): string =
+  return self
+    .selectAvgSql(column)
     .fromSql()
     .joinSql()
     .leftJoinSql()
@@ -176,9 +199,11 @@ proc avgBuilder*(self:Rdb, column:string): Rdb =
     .orderBySql()
     .limitSql()
     .offsetSql()
+    .sqlString
 
-proc sumBuilder*(self:Rdb, column:string): Rdb =
-  return self.selectSumSql(column)
+proc sumBuilder*(self:Rdb, column:string): string =
+  return self
+    .selectSumSql(column)
     .fromSql()
     .joinSql()
     .leftJoinSql()
@@ -194,8 +219,11 @@ proc sumBuilder*(self:Rdb, column:string): Rdb =
     .orderBySql()
     .limitSql()
     .offsetSql()
+    .sqlString
 
-proc columnBuilder*(self:Rdb):Rdb =
-  return self.selectSql()
-        .fromSql()
-        .selectFirstSql()
+proc columnBuilder*(self:Rdb): string =
+  return self
+    .selectSql()
+    .fromSql()
+    .selectFirstSql()
+    .sqlString
