90c5a708d3a1581da3515e7e536b6cd5c26d3889
itsumura-h <39766805+itsumura-h@users.noreply.github.com>
2022-07-17 22:38:49 +0900

    postgre to postgres (#203)

--- src/allographer/baseEnv.nim
+++ src/allographer/baseEnv.nim
@@ -3,7 +3,7 @@ import
   std/strutils
 
 const
-  isExistsSqlite* = when existsEnv("DB_SQLITE"): getEnv("DB_SQLITE").parseBool else: false
-  isExistsPostgre* = when existsEnv("DB_POSTGRE"): getEnv("DB_POSTGRE").parseBool else: false
-  isExistsMysql* = when existsEnv("DB_MYSQL"): getEnv("DB_MYSQL").parseBool else: false
-  isExistsMariadb* = when existsEnv("DB_MARIADB"): getEnv("DB_MARIADB").parseBool else: false
+  isExistsSqlite* = getEnv("DB_SQLITE", $false).parseBool
+  isExistsPostgres* = getEnv("DB_POSTGRES", $false).parseBool
+  isExistsMysql* = getEnv("DB_MYSQL", $false).parseBool
+  isExistsMariadb* = getEnv("DB_MARIADB", $false).parseBool
