0c26d6e56661e066c27eb33089321f12c1681e8f
sgmihai <sgmihai@gmail.com>
2023-06-14 17:45:38 +0300

    initial nim-devel compatibility (#236)

--- src/allographer/schema_builder/queries/generator_utils.nim
+++ src/allographer/schema_builder/queries/generator_utils.nim
@@ -1,5 +1,8 @@
 import strformat
-from db_common import DbError
+when (NimMajor, NimMinor) <= (1, 6):
+  from db_common import DbError
+else:
+  from db_connector/db_common import DbError
 
 proc notAllowed*(option, typ, column:string) =
   raise newException(DbError, &"{option} is not allowed in {typ} column {column}")
