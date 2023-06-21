86a420b9db419c773935e71015235963386c8621
itsumura-h <dumblepy@gmail.com>
2023-06-21 03:47:12 +0000

    enable adminer for sqlite

---
+++ docker/adminer/login-password-less.php
@@ -0,0 +1,9 @@
+<?php
+require_once('plugins/login-password-less.php');
+ 
+/** Set allowed password
+ * @param string result of password_hash
+ */
+return new AdminerLoginPasswordLess(
+    $password_hash = password_hash("pass", PASSWORD_DEFAULT)
+);
