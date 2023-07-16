13e813d8cbeb2d8c81c7eb8fd4f7c250c3e93bf8
itsumura-h <dumblepy@gmail.com>
2023-07-16 16:51:06 +0000

    delete old file

--- src/allographer/schema_builder/queries.bk/query_util.nim
+++
@@ -1,5 +0,0 @@
-import std/strformat
-from std/db_common import DbError
-
-proc notAllowed*(option, typ, column:string) =
-  raise newException(DbError, &"{option} is not allowed in {typ} column {column}")
