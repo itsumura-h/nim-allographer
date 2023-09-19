c88ef05ecad1dfd4dfefcfabb3033f2cb769ff1f
itsumura-h <dumblepy@gmail.com>
2023-09-19 00:21:09 +0000

    mariadb schema test

--- src/allographer/schema_builder/usecases/drop.nim
+++ src/allographer/schema_builder/usecases/drop.nim
@@ -1,6 +1,7 @@
 import std/os
 import ../../query_builder/models/sqlite/sqlite_types
 import ../../query_builder/models/postgres/postgres_types
+import ../../query_builder/models/mariadb/mariadb_types
 import ../../query_builder/models/surreal/surreal_types
 import ../models/table
 import ../enums
@@ -23,6 +24,20 @@ proc drop*(rdb:SqliteConnections, tables:varargs[Table]) =
 
 
 proc drop*(rdb:PostgresConnections, tables:varargs[Table]) =
+  let cmd = commandLineParams()
+  let isReset = defined(reset) or cmd.contains("--reset")
+
+  # create migration table
+  var query = createSchema(rdb, migrationTable)
+  query.createMigrationTable()
+
+  for table in tables:
+    table.usecaseType = Drop
+    query = createSchema(rdb, table)
+    query.dropTable(isReset)
+
+
+proc drop*(rdb:MariadbConnections, tables:varargs[Table]) =
   let cmd = commandLineParams()
   let isReset = defined(reset) or cmd.contains("--reset")
 
