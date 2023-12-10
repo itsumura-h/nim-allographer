029880aaf23c770dba8109bc417cabc727ddcc61
Bung <crc32@qq.com>
2023-12-10 18:13:39 +0800

    fix `getOsName` on `macosx` (#262)

--- src/allographer/v2/query_builder/libs/mariadb/mariadb_rdb.nim
+++ src/allographer/v2/query_builder/libs/mariadb/mariadb_rdb.nim
@@ -1,16 +1,4 @@
-import strutils
-
-func getOsName*():string =
-  when defined(macosx) or defined(linux) or defined(bsd):
-    const f = staticRead("/etc/os-release")
-    for row in f.split("\n"):
-      let kv = row.split("=")
-      if kv[0] == "ID":
-        return kv[1]
-  elif defined(windows):
-    return "windows"
-  else:
-    return ""
+import ../../../../utils
 
 {.push, callconv: cdecl.}
 when defined(nimHasStyleChecks):
