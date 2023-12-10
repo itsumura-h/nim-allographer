77e442259ac40ab67a0702ad2af63eb1790e507f
itsumura-h <dumblepy@gmail.com>
2023-12-10 12:09:52 +0000

    fix migrate example

--- example/migrate.nim
+++ example/migrate.nim
@@ -16,7 +16,7 @@ rdb.create([
     Column.string("oid2").index().nullable(),
     Column.string("Name").nullable(),
     Column.date("birth_date").nullable(),
-    Column.foreign("auth_id").reference("id").on("Auth").onDelete(SET_NULL).default(1)
+    Column.foreign("auth_id").reference("id").onTable("Auth").onDelete(SET_NULL).default(1)
   ]),
 ])
 
@@ -24,17 +24,18 @@ rdb.alter([
   table("Users", [
     # add column
     Column.string("aaa").default("").add(),
-    Column.foreign("bbb").reference("id").on("Auth").onDelete(SET_NULL).add(),
+    Column.foreign("bbb").reference("id").onTable("Auth").onDelete(SET_NULL).add(),
     # change column definition
     Column.string("aaa").nullable().change(),
     # change column name
     Column.renameColumn("aaa", "ccc"),
     # delete column
-    Column.deleteColumn("ccc"),
-    Column.deleteColumn("bbb"),
+    Column.dropColumn("ccc"),
+    Column.dropColumn("bbb"),
   ]),
   # change table name
-  rename("Users", "members"),
-  # drop table
-  drop("members"),
+  table("Users").renameTo("members"),
 ])
+
+# drop table
+rdb.drop(table("members"))
