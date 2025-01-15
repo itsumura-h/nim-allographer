caee8c360e6d6a1866938d2e291b8688b8b50f2b
itsumura-h <dumblepy@gmail.com>
2025-01-15 16:08:31 +0000

    postgres

--- src/allographer/v1/query_builder/models/postgres/postgres_open.nim
+++ src/allographer/v1/query_builder/models/postgres/postgres_open.nim
@@ -5,9 +5,9 @@ import ../../log
 import ./postgres_types
 
 
-proc dbOpen*(_:type PostgreSQL, database: string = "", user: string = "", password: string = "",
-                    host: string = "", port: int = 0, maxConnections: int = 1, timeout=30,
-                    shouldDisplayLog=false, shouldOutputLogFile=false, logDir=""): PostgresConnections =
+proc dbOpen*(_:type PostgreSQL, database: string, user: string, password: string,
+              host: string, port: int, maxConnections=1, timeout=30,
+              shouldDisplayLog=false, shouldOutputLogFile=false, logDir=""): PostgresConnections =
   var conns = newSeq[Connection](maxConnections)
   for i in 0..<maxConnections:
     let conn = postgres_rdb.pqsetdbLogin(host, port.`$`.cstring, nil, nil, database, user, password)
@@ -25,3 +25,19 @@ proc dbOpen*(_:type PostgreSQL, database: string = "", user: string = "", passwo
     pools: pools,
     log: LogSetting(shouldDisplayLog:shouldDisplayLog, shouldOutputLogFile:shouldOutputLogFile, logDir:logDir)
   )
+
+
+proc dbOpen*(_:type PostgreSQL, url: string, maxConnections: int = 1, timeout=30,
+              shouldDisplayLog=false, shouldOutputLogFile=false, logDir=""): PostgresConnections =
+  ## url: "postgresql://user:pass@host:port/database"
+  let isPostgres = url.startsWith("postgresql://")
+  if not isPostgres:
+    raise newException(ValueError, "Invalid URL format. Expected a PostgreSQL URL starting with 'postgresql://'.")
+  
+  let user = url.split("://")[1].split(":")[0]
+  let password = url.split(":")[2].split("@")[0]
+  let host = url.split("@")[1].split(":")[0]
+  let port = url.split(":")[3].split("/")[0]
+  let database = url.split("/")[^1]
+
+  return dbOpen(PostgreSQL, database, user, password, host, port.parseInt, maxConnections, timeout, shouldDisplayLog, shouldOutputLogFile, logDir)
---
+++ tests/v1/postgres/test_open.nim
@@ -0,0 +1,39 @@
+discard """
+  cmd: "nim c $file"
+"""
+
+# nim c -r tests/v1/postgres/test_open.nim
+
+import std/unittest
+import ../../../src/allographer/connection
+import ../../../src/allographer/query_builder
+
+
+suite("PostgreSQL connection"):
+  test("connection"):
+    let database = "database"
+    let user = "user"
+    let password = "pass"
+    let host = "postgres"
+    let port = 5432
+    
+    let rdb = dbOpen(PostgreSQL, database, user, password, host, port)
+    check(rdb.isConnected())
+
+
+  test("connection with URL"):
+    let url = "postgresql://user:pass@postgres:5432/database"
+    let rdb = dbOpen(PostgreSQL, url)
+    check(rdb.isConnected())
+
+
+  test("url is not start with postgresql://"):
+    let url = "mysql://user:pass@postgres:5432/database"
+    expect(ValueError):
+      discard dbOpen(PostgreSQL, url)
+
+
+  test("cannot reach to database"):
+    let url = "postgresql://john:password@host:0001/database"
+    expect(DbError):
+      discard dbOpen(PostgreSQL, url)
