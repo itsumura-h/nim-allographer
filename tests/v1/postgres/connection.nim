7bc67481dce24590179cdaa64ad51f34a860d3e9
itsumura-h <dumblepy@gmail.com>
2025-01-15 23:46:00 +0000

    fix test connection

--- tests/v1/postgres/connection.nim
+++ tests/v1/postgres/connection.nim
@@ -3,12 +3,8 @@ import std/strutils
 import ../../../src/allographer/connection
 
 let
-  database = getEnv("DB_DATABASE")
-  user = getEnv("DB_USER")
-  password = getEnv("DB_PASSWORD")
-  pgHost = getEnv("PG_HOST")
-  pgPort = getEnv("PG_PORT").parseInt
+  pgUrl = getEnv("PG_URL")
   maxConnections = getEnv("DB_MAX_CONNECTION").parseInt
   timeout = getEnv("DB_TIMEOUT").parseInt
 
-let postgres* = dbOpen(PostgreSQL, database, user, password, pgHost, pgPort, maxConnections, timeout, shouldDisplayLog=true)
+let postgres* = dbOpen(PostgreSQL, pgUrl, maxConnections, timeout, shouldDisplayLog=true)
--- tests/v2/mariadb/connection.nim
+++ tests/v2/mariadb/connection.nim
@@ -3,12 +3,8 @@ import std/strutils
 import ../../../src/allographer/connection
 
 let
-  database = getEnv("DB_DATABASE")
-  user = getEnv("DB_USER")
-  password = getEnv("DB_PASSWORD")
-  mariadbHost = getEnv("MARIA_HOST")
-  mysqlPort = getEnv("MY_PORT").parseInt
+  mariadbUrl = getEnv("MARIA_URL")
   maxConnections = getEnv("DB_MAX_CONNECTION").parseInt
   timeout = getEnv("DB_TIMEOUT").parseInt
 
-let mariadb* = dbOpen(MariaDB, database, user, password, mariadbHost, mysqlPort, maxConnections, timeout, shouldDisplayLog=true)
+let mariadb* = dbOpen(MariaDB, mariadbUrl, maxConnections, timeout, shouldDisplayLog=true)
