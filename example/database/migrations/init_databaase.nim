466310a474fd0c35035e0c9926566d83aa7f2122
itsumura-h <dumblepy@gmail.com>
2025-01-17 02:34:52 +0000

    .

--- example/database/migrations/init_databaase.nim
+++ example/database/migrations/init_databaase.nim
@@ -5,19 +5,19 @@ proc initDatabaase*() =
   ## 0001
   rdb.create([
     table("user", [
-      Column.uuid("id").index().comment("ユーザーID"),
-      Column.string("name").comment("ユーザー名"),
-      Column.string("email").comment("ユーザーのメールアドレス"),
-      Column.string("password").comment("ユーザーのパスワード"),
-      Column.integer("created_at").index().comment("作成日時"),
-      Column.integer("updated_at").index().comment("更新日時"),
-    ], "ユーザーテーブル"),
+      Column.uuid("id").index().comment("User ID"),
+      Column.string("name").comment("User name"),
+      Column.string("email").comment("User email address"),
+      Column.string("password").comment("User password"),
+      Column.integer("created_at").index().comment("Created at"),
+      Column.integer("updated_at").index().comment("Updated at"),
+    ], "User table"),
     table("post", [
-      Column.uuid("id").index().comment("投稿ID"),
-      Column.string("title").comment("投稿タイトル"),
-      Column.string("content").comment("投稿内容"),
-      Column.strForeign("user_id").reference("id").onTable("user").comment("ユーザーID"),
-      Column.integer("created_at").index().comment("作成日時"),
-      Column.integer("updated_at").index().comment("更新日時"),
-    ], "投稿テーブル"),
+      Column.uuid("id").index().comment("Post ID"),
+      Column.string("title").comment("Post title"),
+      Column.string("content").comment("Post content"),
+      Column.strForeign("user_id").reference("id").onTable("user").comment("User ID"),
+      Column.integer("created_at").index().comment("Created at"),
+      Column.integer("updated_at").index().comment("Updated at"),
+    ], "Post table"),
   ])
